73e07f93b366485bfe5f846b951551c6ff529d12
killingpandasai-prog <killingpandasai@gmail.com>
2026-03-23 13:20:25 +0530

    Updated app.js with new content on 2026-03-23

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ startBtn.onclick = async () => {
     const source = audioContext.createMediaStreamSource(stream);
     
     // AudioWorklet ని లోడ్ చేయడం
-    await audioContext.audioWorklet.addModule('processor.js');
+    await audioContext.audioWorklet.addModule('processer.js');
     noiseProcessor = new AudioWorkletNode(audioContext, 'studio-processor');
     
     source.connect(noiseProcessor).connect(audioContext.destination);
     
-    statusText.innerText = "మైక్రోఫోన్ కనెక్ట్ అయ్యింది! వాయిస్ వినబడుతుందా?";
+    statusText.innerText = "మైక్రోఫోన్ కనెక్ట్ అయింది! వాయిస్ వినబడుతుందా?";
     learnNoiseBtn.disabled = false;
     startBtn.disabled = true;
 };
@@ -27,5 +27,8 @@ startBtn.onclick = async () => {
 learnNoiseBtn.onclick = () => {
     noiseProcessor.port.postMessage('learn');
     statusText.innerText = "గది నాయిస్‌ని స్టడీ చేస్తున్నాను... 5 సెకన్లు నిశ్శబ్దంగా ఉండండి.";
-    setTimeout(() => { statusText.innerText = "నాయిస్ ప్రొఫైల్ సేవ్ అయ్యింది! ఇప్పుడు మాట్లాడండి."; }, 5000);
+    setTimeout(() => { 
+        noiseProcessor.port.postMessage('stop-learn');
+        statusText.innerText = "నాయిస్ ప్రొఫైల్ సేవ్ అయ్యింది! ఇప్పుడు మాట్లాడండి."; 
+    }, 5000);
 };
